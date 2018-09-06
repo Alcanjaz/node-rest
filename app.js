3f55b8b57a4bf1660de1ce8cb46e21ddb0a00152
Alcanjaz <alexiscaposch15@gmail.com>
2018-09-06 17:25:24 -0300

    third commit

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const producRoutes = require('./api/routes/products');
 const producOrders = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+//routes which should handle request
 app.use('/products', producRoutes);
 app.use('/orders', producOrders);
+
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
