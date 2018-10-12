89c1ec9ad7ee1629f6d9e8bb8629bc4db827759c
Alcanjaz <alexiscaposch15@gmail.com>
2018-10-12 19:16:50 -0300

    A ver

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/rest-shop-db', {useNewUrlParser: true})
+.then(db => console.log('DB connected'))
+.catch(err =>console.error(err));
 
 const producRoutes = require('./api/routes/products');
 const producOrders = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
 
 //routes which should handle request
 app.use('/products', producRoutes);
