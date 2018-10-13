0659e85f08a1fea5e1f5297b516c009887700eef
Alcanjaz <alexiscaposch15@gmail.com>
2018-10-13 10:04:41 -0300

    tb

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    price: Number
+    name: {Type: String, required: true},
+    price: {Type: Number, required: true}
 });
 
 module.exports = mongoose.model("Product", productSchema);
--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
     Product.find()
     .exec()
     .then(docs => {
-        console.log(docs);
-        res.status(200).json(docs);
+        if(docs.length >=0) {
+            res.status(200).json(docs);
+        } else {
+            res.status(404).json({
+                message: 'No entries found'
+            });
+        }
     })
     .catch(err => {
         console.log(err);
@@ -54,14 +59,36 @@ router.get("/:productId", (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message:'updated product'
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({_id: id}, { $set: updateOps})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message:'deleted product'
+    const id = req.params.productId;
+    Product.deleteOne({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
     })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 module.exports = router;
