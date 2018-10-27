89c31347e1ebcf03a7b4bf4419d1366ffb0bbfa0
Alcanjaz <alexiscaposch15@gmail.com>
2018-10-27 04:28:22 -0300

    User login added

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/rest-shop-db', {useNewUrlParser: true})
+mongoose.connect('mongodb://alcanjaz:'+ process.env.MONGO_ATLAS_PW +'@rest-shop-db-shard-00-00-dj5pf.gcp.mongodb.net:27017,rest-shop-db-shard-00-01-dj5pf.gcp.mongodb.net:27017,rest-shop-db-shard-00-02-dj5pf.gcp.mongodb.net:27017/test?ssl=true&replicaSet=rest-shop-db-shard-0&authSource=admin&retryWrites=true',{useNewUrlParser: true})
 .then(db => console.log('DB connected'))
 .catch(err =>console.error(err));
+mongoose.set('useCreateIndex', true);
 
 const producRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
