5788b5ed489ec9b03ab04f72678e1091533640b6
Alcanjaz <alexiscaposch15@gmail.com>
2018-10-27 18:46:33 -0300

    added controllers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://alcanjaz:'+ process.env.MONGO_ATLAS_PW +'@rest-shop-db-shard-00-00-dj5pf.gcp.mongodb.net:27017,rest-shop-db-shard-00-01-dj5pf.gcp.mongodb.net:27017,rest-shop-db-shard-00-02-dj5pf.gcp.mongodb.net:27017/test?ssl=true&replicaSet=rest-shop-db-shard-0&authSource=admin&retryWrites=true',{useNewUrlParser: true})
+mongoose.connect('mongodb://testerano:'+ process.env.MONGO_ATLAS_PW +'@rest-shop-db-shard-00-00-54i8m.gcp.mongodb.net:27017,rest-shop-db-shard-00-01-54i8m.gcp.mongodb.net:27017,rest-shop-db-shard-00-02-54i8m.gcp.mongodb.net:27017/test?ssl=true&replicaSet=rest-shop-db-shard-0&authSource=admin&retryWrites=true',{useNewUrlParser: true})
 .then(db => console.log('DB connected'))
 .catch(err =>console.error(err));
 mongoose.set('useCreateIndex', true);
